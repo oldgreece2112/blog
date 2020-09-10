9aaff542e71a544a0141ea802803447c10eeaaf3
Alex Sofianidis <ansofianidis@outlook.com>
2020-09-09 23:18:55 -0500

    Finished adding middleware for checking ownership

--- middleware/index.js
+++ middleware/index.js
@@ -13,7 +13,31 @@ middlewareObj.isLoggedIn = function(req, res, next){
 
 middlewareObj.checkPostOwnership = function(req, res, next){
     if(req.isAuthenticated()){
-        
+        Post.findById(req.params.id, (err, foundPost) => {
+            if(err){
+                res.redirect("back");
+            }else{
+                if(foundPost.author === req.user.username){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        })
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.id, (err, foundComment) => {
+            if(err){
+                res.redirect("back");
+            }else if(foundComment.author === req.user.username){
+                next();
+            }else{
+                res.rediret("back");
+            }
+        });
     }
 }
 
