b36c8580c62dec25bf3546e6a7ceb8ac44c565fe
Alexander Sofianidis <ansofianidis@outlook.com>
2020-09-26 22:27:34 +0300

    Added the route to create a new comment

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -5,7 +5,34 @@ var Post = require("../models/post.model");
 var router = express.Router();
 
 router.post("/create/:postID", (req, res) => {
+    var id = req.params.id;
 
+    var text = req.body.text;
+
+    var newComment = {
+        post: id,
+        text,
+        author: req.user.username
+    }
+
+    Post.findById(id, (err, foundPost) => {
+        if(err){
+            console.log(err);
+        }else{
+            Comment.create(newComment, (err, newComment) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    foundPost.comments.push({
+                        id: newComment._id,
+                        author: req.user.username
+                    });
+                    foundPost.save();
+                    res.redirect("/posts/view/" + id);
+                }
+            });
+        }
+    });
 });
 
 router.get("/edit/:commentID", (req, res) => {
